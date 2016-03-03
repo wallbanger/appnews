6b4ddb576b3a93a27b9bfaa4be3bd17a05f4a04a
wallbanger <wallbanger@ukr.net>
2016-03-03 17:01:57 +0200

    selected with callback func

--- src/app.js
+++ src/app.js
@@ -3,21 +3,54 @@ import React from 'react'
 import articles from './fixtures'
 
 class Article extends React.Component {
+    state = {
+        showBody: false
+    };
+
     render() {
         const { title, body } = this.props.article
+        const bodyElement = this.state.showBody ? <p>{body}</p> : null
+        const selectedStyle = this.props.selected ? {color: 'red'} : null
         return (
             <div>
-                <h3>{title}</h3>
-                <p>{body}</p>
+                <h3
+                    style = {selectedStyle}
+                    onClick={this.handleClick.bind(this)}>{title}</h3>
+                <a href = "#" onClick = {this.select.bind(this)}>select</a>
+                {bodyElement}
             </div>
         )
+    }
+
+    select(ev) {
+        ev.preventDefault()
+        this.props.select()
+    }
+
+    handleClick(ev) {
+        this.setState({
+            showBody: !this.state.showBody
+        })
     }
 }
 
 class ArticleList extends React.Component {
+    state = {
+        selected: []
+    };
+
+    static propTypes = {
+        articles: React.PropTypes.array
+    };
+
     render() {
         const articles = this.props.articles.map((article) =>
-        <li><Article article={article} /></li>)
+        <li key={article.id}>
+            <Article
+                article={article}
+                select = {this.select(article.id).bind(this)}
+                selected = {this.state.selected.includes(article.id)} />
+        </li>)
 
         return(
             <div>
@@ -26,6 +59,14 @@ class ArticleList extends React.Component {
                 </ul>
             </div>
         )
+    }
+
+    select(id) {
+        return function() {
+            this.setState({
+                selected: this.state.selected.concat(id)
+            })
+        }
     }
 }
 
